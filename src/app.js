40c61bdb6f61573648b6ca0a1a75f6afea85a5d1
Nayan Kar <167174966+nkar90@users.noreply.github.com>
2025-12-10 23:20:41 +0530

    update backend app.js netlify support

--- src/app.js
+++ src/app.js
@@ -1,46 +1,31 @@
 const express = require('express');
+const serverless = require("serverless-http");
 const app = express();
 const connectDB = require('./config/database');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const http = require('http');
 require('dotenv').config();
 
-// CORS FIX FOR RENDER + LOCAL
 app.use(cors({
-  origin: [ "http://localhost:5173", process.env.FRONTEND_URL ],
+  origin: ["http://localhost:5173","https://techtribe-delta.vercel.app"],
   credentials: true,
 }));
 
 app.use(express.json());
 app.use(cookieParser());
 
-const authRouter = require('./routes/auth');
-const profileRouter = require('./routes/profile');
-const requestRouter = require('./routes/request');
-const userRouter = require('./routes/user');
-const chatRouter = require('./routes/chat');
-const initializeSocket = require('./utils/socket');
+app.use("/", require('./routes/auth'));
+app.use("/", require('./routes/profile'));
+app.use("/", require('./routes/request'));
+app.use("/", require('./routes/user'));
+app.use("/", require('./routes/chat'));
 
-app.use("/", authRouter);
-app.use("/", profileRouter);
-app.use("/", requestRouter);
-app.use("/", userRouter);
-app.use("/", chatRouter);
+connectDB();
 
-const server = http.createServer(app);
-initializeSocket(server);
-
-const PORT = process.env.PORT || 5000;
-
-// CONNECT DATABASE AND START SERVER
-connectDB()
-  .then(() => {
-    console.log("Database connected successfully");
-    server.listen(PORT, () => {
-      console.log("Server running on port:", PORT);
-    });
-  })
-  .catch((err) => {
-    console.log("Database connection failed", err);
-  });
+// IMPORTANT!
+if (process.env.NETLIFY) {
+  module.exports.handler = serverless(app);
+} else {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log("Server running:", PORT));
+}
