5c97a23fb9d9c5064307891744f0335a35f55089
Nayan Kar <167174966+nkar90@users.noreply.github.com>
2025-12-13 12:05:07 +0530

    updated backend/frontend fixes

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,27 @@
 
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//     try {
+//         // console.log("DB_CONNECTION_SECRET:", process.env.DB_CONNECTION_SECRET); // Debug
+//         await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+//         console.log("MongoDB connected successfully");
+//     } catch (err) {
+//         console.error("Database connection failed:", err);
+//     }
+// };
+
+// module.exports = connectDB;
+
+
+
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        // console.log("DB_CONNECTION_SECRET:", process.env.DB_CONNECTION_SECRET); // Debug
-        await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+        console.log("Loaded Mongo URI:", process.env.MONGO_URI); // Debug
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected successfully");
     } catch (err) {
         console.error("Database connection failed:", err);
@@ -12,5 +29,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-
